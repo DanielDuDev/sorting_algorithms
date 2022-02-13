6302d63f41a993cb4120fc78bf6e822c14d9c06f
daniel <danydu1299@gmail.com>
2022-02-13 18:24:50 -0500

    Readme, task 1, bubble sort algorinth

---
+++ 0-bubble_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * swap - swap the values in the array
+ *
+ * @x: value array[i]
+ * @y: value array[i+1]
+ * Return: Nothing
+ */
+
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+ * bubble_sort - sort an array
+ *
+ * @array: array to sorted
+ * @size: size of the array
+ *
+ * Return: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	size_t j = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+			}
+			print_array(array, size);
+		}
+	}
+}
+
