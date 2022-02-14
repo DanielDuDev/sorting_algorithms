b95ced460368b7f722b0e39f61551cd5ba170542
daniel <danydu1299@gmail.com>
2022-02-14 14:42:28 -0500

    insert algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,62 +3,62 @@
 /**
  * size_list - count the size of the linked list
  *
- * @list - double linked list
+ * @list: double linked list
  * Return: size
  */
 int size_list(listint_t *list)
 {
-    int i = 0;
-    if (list)
-    {
-        while (list)
-        {
-            list = list->next;
-            i++;
-        }
-    }
-    return (i);
+	int i = 0;
+
+	if (list)
+	{
+		while (list)
+		{
+			list = list->next;
+			i++;
+		}
+	}
+	return (i);
 }
 
 /**
  * insertion_sort_list - sort with insertion method
  *
- * @list: souble linked list
+ * @list: double linked list
  * Return: Nothing
  */
 
 void insertion_sort_list(listint_t **list)
 {
-    /*int size = 0;*/
+	listint_t *h = *list, *current = NULL, *head = NULL, *phead = NULL;
 
-    listint_t *h = *list, *current = NULL, *head = NULL, *phead = NULL;
-    if (h && h->next)
-    {
-        while (h != NULL)
-        {
-            current = h;
-            h = h->next;
-            if (head == NULL || current->n < head->n)
-            {
-                /*insert inte the head of ther sorted list*/
-                current->next = head;
-                head = current;
-            }
-            else
-            {
-                phead = head;
-                while (phead != NULL)
-                {
-                    if (phead->next == NULL || current->n < phead->next->n)
-                    {
-                        current->next = phead->next;
-                        phead->next = current;
-                        break;
-                    }
-                    phead = phead->next;
-                }
-            }
-        }
-    }
-    *list = head;
+	if (h && h->next)
+	{
+		while (h != NULL)
+		{
+			current = h;
+			h = h->next;
+			if (head == NULL || current->n < head->n)
+			{
+				/*insert inte the head of ther sorted list*/
+				current->next = head;
+				head = current;
+			}
+			else
+			{
+				phead = head;
+				while (phead != NULL)
+				{
+					if (phead->next == NULL || current->n < phead->next->n)
+					{
+						current->next = phead->next;
+						phead->next = current;
+						break;
+					}
+					phead = phead->next;
+				}
+			}
+		}
+	}
+	*list = head;
 }
